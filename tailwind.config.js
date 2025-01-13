1b255e370b48bade50b99d544d67f269dde433cd
Amil Muhammed Hamza <amilmohd155@gmail.com>
2025-01-13 19:55:04 +0000

    feat: :tada: Added nativewind and removed template files

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/app/**/*.{js,jsx,ts,tsx}",
+    "./src/components/**/*.{js,jsx,ts,tsx}",
+  ],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {
+      aspectRatio: {
+        card: "1.586",
+      },
+    },
+  },
+};
